c6861b1fbe3a5e9da8219a9e30666ade54cc1e3f
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2017-07-01 16:43:21 +0530

    Random access File

---
+++ src/file/RandomFile.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+public class RandomFile {
+	public static void main(String[] args)throws IOException {
+		RandomAccessFile rwfile=null;
+		File f=new File("hello.txt");
+		try {
+			rwfile =new RandomAccessFile(f,"rw");
+			rwfile.writeChar('c');
+			rwfile.writeDouble(3.1412);
+			rwfile.writeInt(1212);
+			rwfile.seek(0);
+			//reading file
+			System.out.println(rwfile.readChar()+"\n"+rwfile.readDouble()+"\n"+rwfile.readInt());
+			rwfile.seek(rwfile.length());
+			rwfile.writeBoolean(false);
+			System.out.println("Reaing Last Location");
+			rwfile.seek(3);
+			System.out.println(rwfile.readBoolean());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
