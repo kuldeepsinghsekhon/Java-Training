e9a9738163f811184993e263b89ba507bf7b5889
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2017-07-01 17:28:48 +0530

    Merge two Streams By SequenceInputStream

---
+++ src/file/SequenceMerge.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+public class SequenceMerge {
+    public static void main(String[] args) throws IOException {
+			FileInputStream fis1 = new FileInputStream("RandomFile.java");
+           FileInputStream  fis2 = new FileInputStream("WordByWord.java");
+            FileOutputStream out = new FileOutputStream("sequenceout.txt");
+			SequenceInputStream sis= new SequenceInputStream(fis1,fis2);
+            int c;
+            while ((c = sis.read()) != -1) {
+                out.write(c);
+            }
+    }
+}
