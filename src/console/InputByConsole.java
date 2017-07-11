9cfb278755b3c5a3511fbc1385857ac0813f8963
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2017-07-11 13:57:58 +0530

    Input from Console using Console class

--- src/console/InputByConsole.java
+++ src/console/InputByConsole.java
@@ -7,6 +7,9 @@ public class InputByConsole {
 			System.err.println("No console.");
 			System.exit(1);
 		}
+		System.out.println("Enter password");
+		String s=new String(c.readPassword());
+		System.out.println("password is "+ s);
 		i = Integer.parseInt(c.readLine("Enter value of I"));
 		j = Integer.parseInt(c.readLine("Enter value of I"));
 		System.out.println((i < j) ? "i is less than j" : "i is greater than j");
