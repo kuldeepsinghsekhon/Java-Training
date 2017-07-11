aae85575289c8a4493eb1c528b4655f9d745d1f8
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2017-07-11 15:28:22 +0530

    Ifelse Demo added

---
+++ src/control/Ifelse.java
@@ -0,0 +1,11 @@
+package Ifelse;
+public class Ifelse {
+	public static void main(String arg[]) {
+		int marks = 90;
+		if (marks >= 40) {
+			System.out.println("your are pass");
+		} else {
+			System.out.println("your are fail");
+		}
+	}
+}
