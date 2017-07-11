25c30a9801a5a52143908dc76ef08e0230a5a9fc
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2017-07-11 15:49:32 +0530

    Create elseif

---
+++ src/control/Elseif.java
@@ -0,0 +1,15 @@
+package control;
+class Elseif{
+public static void main(String arg[]) {
+		int marks = 80;
+		if (marks > 70) {
+			System.out.println("your Grade Is A");
+		} else if (marks > 60) {
+			System.out.println("your Grade Is B");
+		} else if (marks > 40) {
+			System.out.println("your Grade Is C");
+		} else if (marks > 20) {
+			System.out.println("Beta G your Are Fail");
+		}
+	}
+}
