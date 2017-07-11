45c7467c5eea7fd2d76ac4659840b22b5b9e2c31
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2017-07-11 16:31:43 +0530

    create ForLoopAdvance

---
+++ src/control/ForLoopAdvance.java
@@ -0,0 +1,11 @@
+package control;
+class ForLoopAdvance{
+	public static void main(String args[]) {
+		int arr[] = { 12, 13, 14, 44 };
+
+		for (int i : arr) {
+			System.out.println(i);
+		}
+
+	}
+}
